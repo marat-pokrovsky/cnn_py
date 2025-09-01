028ba991bd38478b81ec68aa079b6615ecf54c79
marat-pokrovsky <175550684+marat-pokrovsky@users.noreply.github.com>
2025-09-01 00:00:00 +0000

    Add model

---
+++ model.py
@@ -0,0 +1,20 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+
+def create_model(num_classes):
+    """
+    Creates a simple CNN model.
+    """
+    model = keras.Sequential(
+        [
+            keras.Input(shape=(28, 28, 1)),
+            layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Flatten(),
+            layers.Dropout(0.5),
+            layers.Dense(num_classes, activation="softmax"),
+        ]
+    )
+    return model
